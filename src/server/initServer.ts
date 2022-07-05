eb1900d73a3754378b87e54eeb73cfea1bac8385
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-05 22:40:11 +0200

    Configure server properly

--- src/server/initServer.ts
+++ src/server/initServer.ts
@@ -1,20 +1,21 @@
 import config from 'config';
 import morgan from 'morgan';
 import express from 'express';
-import { Express } from 'express';
+import { Express, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 import { log } from '@root/utils';
+import msgRouter from '@root/server/routes/message.routes';
 
-const initServer: () => Promise<void> = async () => {
-	const PORT = <number>config.get('PORT') || 7102;
-	const app: Express = express();
+const PORT = <number>config.get('PORT') || 7102;
+const app: Express = express();
 
-	const loggerStream = {
-		write: (text: string) => {
-			log.info(text);
-		},
-	};
+const loggerStream = {
+	write: (text: string) => {
+		log.info(text);
+	},
+};
+const initServer = () => {
 	app.use(morgan('combined', { stream: loggerStream }));
 	app.use(
 		cors({
@@ -29,6 +30,20 @@ const initServer: () => Promise<void> = async () => {
 			optionsSuccessStatus: 200,
 		}),
 	);
+	app.use(express.json());
+
+	// use the created routes to
+	app.use('/api/v1/messages', msgRouter);
+	app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
+		res.status(err.status || 500);
+		res.send({
+			error: {
+				status: err.status || 500,
+				message: err.message,
+			},
+		});
+		next();
+	});
 
 	app.listen(PORT, () => {
 		log.info(`message-service listening on port ${PORT}`);
