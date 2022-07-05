55702ed85f6c4e4c0f437ed8c7f36979a523da9f
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-05 22:42:33 +0200

    Add config

--- config/default.ts
+++ config/default.ts
@@ -1,2 +1,3 @@
 export const PORT = 7102;
 export const CONNECTION_URL = 'mongodb://nubian_user:W4R10ckS3cret@message-service-db:27017/?authSource=admin';
+export const ACCESS_TOKEN_SECRET = '96ee5c7254a19109a0b9606cf04a58950992b59443b3334c0f55639abc780aed';
