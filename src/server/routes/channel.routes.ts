e86a50933dfeca67f6a608bc3bed9115a0d9de07
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-23 10:31:42 +0200

    Update router

---
+++ src/server/routes/channel.routes.ts
@@ -0,0 +1,12 @@
+import { middleware } from '../middleware';
+import { Router } from 'express';
+import { channelController } from '@root/server/controllers';
+
+const channelRoutes: Router = Router();
+channelRoutes.post('/', middleware.verifyAccessToken, channelController.createChannel);
+channelRoutes.get('/', middleware.verifyAccessToken, channelController.getChannels);
+channelRoutes.get('/:id', middleware.verifyAccessToken, channelController.getMessage);
+channelRoutes.patch('/:id', middleware.verifyAccessToken, channelController.updateChannel);
+channelRoutes.delete('/:id', middleware.verifyAccessToken, channelController.deleteChannel);
+
+export default channelRoutes;
--- src/server/routes/message.routes.ts
+++ src/server/routes/message.routes.ts
@@ -1,12 +1,12 @@
-import { verifyAccessToken } from './../middleware/index';
+import { middleware } from '../middleware';
 import { Router } from 'express';
 import { messageController } from '@root/server/controllers';
 
 const msgRouter: Router = Router();
-msgRouter.post('/', verifyAccessToken, messageController.sendMessage);
-msgRouter.get('/', verifyAccessToken, messageController.getMessages);
-msgRouter.get('/:id', verifyAccessToken, messageController.getMessage);
-msgRouter.patch('/:id', verifyAccessToken, messageController.updateMessage);
-msgRouter.delete('/:id', verifyAccessToken, messageController.deleteMessage);
+msgRouter.post('/', middleware.verifyAccessToken, messageController.sendMessage);
+msgRouter.get('/', middleware.verifyAccessToken, messageController.getMessages);
+msgRouter.get('/:id', middleware.verifyAccessToken, messageController.getMessage);
+msgRouter.patch('/:id', middleware.verifyAccessToken, messageController.updateMessage);
+msgRouter.delete('/:id', middleware.verifyAccessToken, messageController.deleteMessage);
 
 export default msgRouter;
