e8ac5e580b87919d627c034d8ef3c0fe6fd319fa
Nkhatho Ziggy Mahlomaholo <ziggleton@icloud.com>
2022-07-24 14:56:57 +0200

    Add Helper

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,2 +1,3 @@
 export { default as messageController } from './message.controller';
 export { default as channelController } from './channel.controller';
+export { default as signatureController } from './signature.controller';
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export { default as log } from './logger';
+export { default as s3SignatureHelper } from './s3/s3.helper';
